c02c702780336411e6a829fc06a4a5f579d5a368
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2025-01-12 12:05:38 +0400

    Update main.py

--- Simple Library System/main.py
+++ Simple Library System/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 from app.books.books_router import router as books_router
+from config import settings
+
+PORT = settings.PORT
 
 app = FastAPI()
 app.include_router(books_router)
 
 if __name__ == '__main__':
-  uvicorn.run('main:app', port=3001, reload=True)
+  uvicorn.run('main:app', port=PORT, reload=True)
   
